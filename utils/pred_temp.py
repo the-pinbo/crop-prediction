129057b8c20a589b259396ba03e74e0cf36117c7
inba <inba2002.p@gmail.com>
2023-02-11 18:27:16 +0530

    working temperature function

--- utils/pred_temp.py
+++ utils/pred_temp.py
@@ -1,3 +1,8 @@
+import pandas as pd
+
+
 def get_temperature(state, district, month):
-    temperature = 0
+    df = pd.read_csv('data/district wise rainfall normal.csv')
+    row = df[(df['STATE_UT_NAME'] == state) & (df['DISTRICT'] == district)]
+    temperature = row[month].values[0]
     return temperature
