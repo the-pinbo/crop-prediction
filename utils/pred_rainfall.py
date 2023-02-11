88883e0739ef6d2d646d7cdb64e34b5c7c98d2a0
Pranav Koundinya <78913261+pranavmkoundinya@users.noreply.github.com>
2023-02-11 16:17:42 +0000

    Corrected the temperature/rainfall mixup

--- utils/pred_rainfall.py
+++ utils/pred_rainfall.py
@@ -1,3 +1,5 @@
 def get_rainfall(state, district, month):
-    rainfall = 0
+    df = pd.read_csv('data/district wise rainfall normal.csv')
+    row = df[(df['STATE_UT_NAME'] == state) & (df['DISTRICT'] == district)]
+    rainfall = row[month].values[0]
     return rainfall
--- utils/pred_temp.py
+++ utils/pred_temp.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def get_temperature(state, district, month):
-    df = pd.read_csv('data/district wise rainfall normal.csv')
-    row = df[(df['STATE_UT_NAME'] == state) & (df['DISTRICT'] == district)]
-    temperature = row[month].values[0]
+    # df = pd.read_csv('data/district wise rainfall normal.csv')
+    # row = df[(df['STATE_UT_NAME'] == state) & (df['DISTRICT'] == district)]
+    # temperature = row[month].values[0]
     return temperature
