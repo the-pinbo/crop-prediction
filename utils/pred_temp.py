4a840a3fcb21c7dd94520f89f8bf2bc96a22595d
Pranav Koundinya <78913261+pranavmkoundinya@users.noreply.github.com>
2023-02-11 08:57:47 +0000

    Rainfall done

--- utils/pred_temp.py
+++ utils/pred_temp.py
@@ -1,3 +1,11 @@
+import pandas as pd
+import numpy as np
+
 def get_temperature(state, district, month):
-    temperature = 0
+    # state_district = df[df['STATE_UT_NAME'].values == state and df['DISTRICT'].values == district]
+    # index = int(np.argwhere(df['STATE_UT_NAME'].values == state and df['DISTRICT'].values == district))
+    df = pd.read_csv('data/district wise rainfall normal.csv')
+    # index = int(np.intersect1d(np.argwhere(df['STATE_UT_NAME'].values == state), np.argwhere(df['DISTRICT'].values == district)))
+    # temperature = df[month].values[index] 
+       
     return temperature
