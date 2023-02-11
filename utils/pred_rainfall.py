7f6efa5acef85138ffc8e6bc8dcf511965b4ff5d
Pranav Koundinya <78913261+pranavmkoundinya@users.noreply.github.com>
2023-02-11 16:14:31 +0000

    Corrected temperature/rainfall mixup

--- utils/pred_rainfall.py
+++ utils/pred_rainfall.py
@@ -1,3 +1,5 @@
 def get_rainfall(state, district, month):
-    rainfall = 0
+    df = pd.read_csv('data/district wise rainfall normal.csv')
+    index = int(np.intersect1d(np.argwhere(df['STATE_UT_NAME'].values == state), np.argwhere(df['DISTRICT'].values == district)))
+    rainfall = df[month].values[index] 
     return rainfall
--- utils/pred_temp.py
+++ utils/pred_temp.py
@@ -4,8 +4,8 @@ import numpy as np
 def get_temperature(state, district, month):
     # state_district = df[df['STATE_UT_NAME'].values == state and df['DISTRICT'].values == district]
     # index = int(np.argwhere(df['STATE_UT_NAME'].values == state and df['DISTRICT'].values == district))
-    df = pd.read_csv('data/district wise rainfall normal.csv')
-    index = int(np.intersect1d(np.argwhere(df['STATE_UT_NAME'].values == state), np.argwhere(df['DISTRICT'].values == district)))
-    temperature = df[month].values[index] 
+    # df = pd.read_csv('data/district wise rainfall normal.csv')
+    # index = int(np.intersect1d(np.argwhere(df['STATE_UT_NAME'].values == state), np.argwhere(df['DISTRICT'].values == district)))
+    # temperature = df[month].values[index] 
        
     return temperature
